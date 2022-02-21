44fbef12abee9c92e2795f7b09942f6176a94a90
grzegorzswistak <grzegorz.swistak2@gmail.com>
2022-02-21 09:47:17 +0100

    Headers

--- src/test/java/com/orange/lo/sample/mqtt2eventhub/evthub/EventHubPropertiesTest.java
+++ src/test/java/com/orange/lo/sample/mqtt2eventhub/evthub/EventHubPropertiesTest.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) Orange. All Rights Reserved.
+ * <p>
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 package com.orange.lo.sample.mqtt2eventhub.evthub;
 
 import org.junit.jupiter.api.BeforeEach;
